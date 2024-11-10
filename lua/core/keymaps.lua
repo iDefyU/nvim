81aecf3aaf659b6e934d13a967a68496178e84c4
Ryker <ryker@Rykerui-MacBookPro.local>
2024-11-10 22:09:44 +0900

    .

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,24 +1,40 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 local opts = { noremap = true, silent = true }
 
-vim.keymap.set('n', 'x', '"_x', opts);
+vim.keymap.set("n", "x", '"_x', opts)
 
+vim.keymap.set("n", "n", "nzzzv", opts)
+vim.keymap.set("n", "N", "Nzzzv", opts)
 
-vim.keymap.set('n', 'n', 'nzzzv', opts)
-vim.keymap.set('n', 'N', 'Nzzzv', opts)
+vim.keymap.set("n", "<C-d>", "<C-d>zz", opts)
+vim.keymap.set("n", "<C-u>", "<C-u>zz", opts)
 
-vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
-vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
-
-vim.keymap.set('v', 'p', '"_dp', opts)
+vim.keymap.set("v", "p", '"_dp', opts)
 
 -- Bufferline Tab
-vim.keymap.set('n', '<C-l>', ':bnext<CR>', opts)
-vim.keymap.set('n', '<C-h>', ':bprevious<CR>', opts)
+vim.keymap.set("n", "<Tab>", ":bnext<CR>", opts)
+vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", opts)
+vim.keymap.set("n", "<leader>x", ":Bdelete!<CR>", opts)
+vim.keymap.set("n", "<leader>b", "<cmd> enew <CR>", opts)
 
+vim.keymap.set("n", "<leader>lw", "<cmd>set wrap!<CR>", opts)
 
-vim.keymap.set('n', '<leader>lw', '<cmd>set wrap!<CR>', opts)
+-- Navigate between splits
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>", opts)
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>", opts)
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>", opts)
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>", opts)
+
+-- Stay in indent mode
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
+
+-- Diagnostic keymaps
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
