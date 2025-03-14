dbf088e5d9159a491e2ba6fddc6b8388c45dd694
Ryker <ryker@Rykerui-MacBookPro.local>
2025-03-15 06:45:45 +0900

    1

--- lua/plugins/flutter-tool.lua
+++ lua/plugins/flutter-tool.lua
@@ -5,27 +5,5 @@ return {
     'nvim-lua/plenary.nvim',
     'stevearc/dressing.nvim', -- optional for vim.ui.select
   },
-  config = function()
-    require('flutter-tools').setup {
-      widget_guides = {
-        -- enabled = true,
-      },
-      ui = {
-        notification_style = 'native',
-      },
-      lsp = {
-        color = {
-          virtual_text = true,
-        },
-      },
-      on_attach = function() end,
-      messages = function() end,
-
-      settings = {
-        lineLength = 120,
-        indentStyle = 'tab', -- 'space' 또는 'tab'
-        indentWidth = 4,
-      },
-    }
-  end,
+  config = true,
 }
